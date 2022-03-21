3f0df9382814f61a6abcbb02f54df250b72cd4f4
reichtergen <reichtergen.reinhard+github@gmail.com>
2022-03-21 23:05:04 +0300

    update

--- main.c
+++ main.c
@@ -8,12 +8,18 @@ void func( void ) {
     printf("hello timer!\n");
 }
 
+void func2( void ) {
+    printf("Goodbye timer!\n");
+}
+
 int main( void ) {
 
-    stimer_t timer;
+    stimer_t timer, timer2;
 
     init_timer(&timer, 1000, func);
+    init_timer(&timer2, 500, func2);
 
+    start_timer(&timer2);
     start_timer(&timer);
 
     printf("**** timer started! ****\n");
@@ -21,6 +27,7 @@ int main( void ) {
     Sleep(10000); /* Wait for 10 sec */
 
     stop_timer(&timer);
+    stop_timer(&timer2);
     printf("**** timer stopped! ****\n");
 
     getchar(); 
